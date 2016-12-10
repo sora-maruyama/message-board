20cf8fdf2403e522d44b82236893e43adbbf2f52
sora maruyama <sorahonda2@yahoo.co.uk>
2016-12-10 15:03:38 +0000

    challenge2

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ActiveRecord::Base
 
   # 年齢(age)に 0以上かつ150以下の数字 だけ受け付ける
-  validates :age , length: { minimum: 0, maximum: 150 } ,numericality: true, presence: true
+  validates :age , numericality:{ only_integer: true, greater_than_or_equal_to:0, less_than_or_equal_to:150 } 
+
 end

32836aafabc57b6daa42f6976466ca78ef9a6419
sora maruyama <sorahonda2@yahoo.co.uk>
2016-12-10 16:25:20 +0000

    error

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,9 +3,8 @@ class Message < ActiveRecord::Base
   validates :name , length: { maximum: 20 } , presence: true
   
   #年齢(age)に 0以上かつ150以下の数字 だけ受け付ける
-
-   validates :age ,numericality: {only_integer: true,  greater_than_or_equal_to: 0, less_than_or_equal_to:150 }
-   
+ validates :age ,numericality: {only_integer: true,  greater_than_or_equal_to: 0, less_than_or_equal_to:150 }
+  
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
 end
